e5b50d767e9cd1f50bfa4a39a7224f48adfa1f68
codewithphred <owoyemiwilfred@gmail.com>
2022-09-12 02:40:48 -0700

    newest

--- src/home.jsx
+++ src/home.jsx
@@ -4,7 +4,7 @@ function Home () {
  return (
     <div id="home" className="about">
     <h1>WELCOME</h1>
-   <h3>I,M Wilfred, <span>A FrontEnd Web developer</span>  </h3>    
+   <h3>I'M Wilfred, <span>A FrontEnd Web developer</span>  </h3>    
    </div>
  )   
 }
